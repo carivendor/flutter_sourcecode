30645fa8bfed75480c418c85e8fa4cc8bb3d895b
luckysendouw <53346547+luckysendouw@users.noreply.github.com>
2019-07-28 08:49:42 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding/pages/AuthPages.dart';
+import 'package:flutter_onboarding/provider/auth.dart';
+
+import 'package:provider/provider.dart';
+// import 'package:flutter_onboarding/pages/onboarding.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
+        // ChangeNotifierProvider.value(
+        //   value: Products(),
+        // ),
+        // ChangeNotifierProvider.value(
+        //   value: Cart(),
+        // ),
+        // ChangeNotifierProvider.value(
+        //   value: Orders(),
+        // ),
+      ],
+      child: MaterialApp(
+          // title: 'MyShop',
+          // theme: ThemeData(
+          //   primarySwatch: Colors.purple,
+          //   accentColor: Colors.deepOrange,
+          //   fontFamily: 'Lato',
+          // ),
+          home: AuthScreen(),
+          routes: {
+            // ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            // CartScreen.routeName: (ctx) => CartScreen(),
+            // OrdersScreen.routeName: (ctx) => OrdersScreen(),
+            // UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+            // EditProductScreen.routeName: (ctx) => EditProductScreen(),
+          }),
+    );
+  }
+}
+
---
+++ lib/pages/AuthPages.dart
@@ -0,0 +1,291 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding/provider/auth.dart';
+import 'package:provider/provider.dart';
+import '../models/http_exception.dart';
+
+enum AuthMode { Signup, Login }
+
+class AuthScreen extends StatelessWidget {
+  static const routeName = '/auth';
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    // final transformConfig = Matrix4.rotationZ(-8 * pi / 180);
+    // transformConfig.translate(-10.0);
+    return Scaffold(
+      // resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(0, 27, 57, 1.0),
+              image: DecorationImage(
+                image: AssetImage("assets/TransparentBackground.png"),
+                fit: BoxFit.cover,
+              ),
+              // gradient: LinearGradient(
+              //   colors: [
+              //     Color.fromRGBO(215, 117, 255, 1).withOpacity(0.5),
+              //     Color.fromRGBO(255, 188, 117, 1).withOpacity(0.9),
+              //   ],
+              //   begin: Alignment.topLeft,
+              //   end: Alignment.bottomRight,
+              //   stops: [0, 1],
+              // ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              height: deviceSize.height,
+              width: deviceSize.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        bottom: 80,
+                        left: 110,
+                        right: 110,
+                        top: 80,
+                      ),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/LogoAuthScreen.png"),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: deviceSize.width > 600 ? 2 : 1,
+                    child: AuthCard(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AuthCard extends StatefulWidget {
+  const AuthCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _AuthCardState createState() => _AuthCardState();
+}
+
+class _AuthCardState extends State<AuthCard> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  AuthMode _authMode = AuthMode.Login;
+  Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+  var _isLoading = false;
+  final _passwordController = TextEditingController();
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('An Error Occured!'),
+        content: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Okay'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> _submit() async {
+    if (!_formKey.currentState.validate()) {
+      // Invalid!
+      return;
+    }
+    _formKey.currentState.save();
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      if (_authMode == AuthMode.Login) {
+        // Log user in
+        await Provider.of<Auth>(context, listen: false).login(
+          _authData['email'],
+          _authData['password'],
+        );
+      } else {
+        // Sign user up
+        await Provider.of<Auth>(context, listen: false).signup(
+          _authData['email'],
+          _authData['password'],
+        );
+      }
+    } on HttpException catch (error) {
+      var errorMessage = 'Aunthetication Failed';
+      if (error.toString().contains('EMAIL_EXISTS')) {
+        errorMessage = 'Email yang anda masukan sudah terpakai.';
+      } else if (error.toString().contains('INVALID_EMAIL')) {
+        errorMessage = 'Email yang dimasukan tidak valid';
+      } else if (error.toString().contains('WEAK_PASSWORD')) {
+        errorMessage = 'password yang dimasukan terlalu lemah';
+      } else if (error.toString().contains('EMAIL_NOT_FOUND')) {
+        errorMessage = 'tidak dapat menemukan pengguna dengan email tersebut';
+      } else if (error.toString().contains('INVALID_PASSWORD')) {
+        errorMessage = 'Invalid Password';
+      }
+      _showErrorDialog(errorMessage);
+    } catch (error) {
+      var errorMessage =
+          'Could not Auntheticate for now, please try again later';
+      _showErrorDialog(errorMessage);
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _switchAuthMode() {
+    if (_authMode == AuthMode.Login) {
+      setState(() {
+        _authMode = AuthMode.Signup;
+      });
+    } else {
+      setState(() {
+        _authMode = AuthMode.Login;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+    return Card(
+      color: Color.fromRGBO(0, 0, 0, 0.0),
+      elevation: 0.0,
+      child: Container(
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.85,
+        padding: EdgeInsets.all(15.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter Your E-Mail',
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value.isEmpty || !value.contains('@')) {
+                      return 'Invalid email!';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _authData['email'] = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter Your Password',
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                  obscureText: true,
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 5) {
+                      return 'Password is too short!';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _authData['password'] = value;
+                  },
+                ),
+                if (_authMode == AuthMode.Signup)
+                  TextFormField(
+                    enabled: _authMode == AuthMode.Signup,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      labelStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    obscureText: true,
+                    validator: _authMode == AuthMode.Signup
+                        ? (value) {
+                            if (value != _passwordController.text) {
+                              return 'Passwords do not match!';
+                            }
+                            return null;
+                          }
+                        : null,
+                  ),
+                SizedBox(
+                  height: 15,
+                ),
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  RaisedButton(
+                    child: Text(
+                      _authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: _submit,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 90.0, vertical: 8.0),
+                    color: Color.fromRGBO(237, 195, 111, 1.0),
+                    textColor: Color.fromRGBO(0, 27, 57, 1.0),
+                    elevation: 3.0,
+                  ),
+                FlatButton(
+                  child: Text(
+                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} VENDOR'),
+                  onPressed: _switchAuthMode,
+                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  textColor: Theme.of(context).primaryColor,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
